88f4d72c6f52fadd301a8c115f6d38035191d432
Ivan G <mr.zero@inbox.ru>
2023-07-07 21:33:37 +0300

    init: setup async race project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const EslintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new EslintPlugin({ extensions: 'ts' }),
+  ],
+};
